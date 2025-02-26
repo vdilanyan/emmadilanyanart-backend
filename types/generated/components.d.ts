f9cc935d3ab03d35f8ec1ff211c4ed7e70058c18
vladimir <vladimir.dilanyan@raisin.com>
2025-02-26 15:39:22 +0400

    first commit

---
+++ types/generated/components.d.ts
@@ -0,0 +1,21 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface VideosVideos extends Struct.ComponentSchema {
+  collectionName: 'components_videos_videos';
+  info: {
+    description: '';
+    displayName: 'Videos';
+    icon: 'play';
+  };
+  attributes: {
+    youtube_video_id: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'videos.videos': VideosVideos;
+    }
+  }
+}
